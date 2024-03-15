3c596ad701a3a61677508c8c3e2b4f8b0b2afd87
Shakil Ahmed <ahmedshakil00071@gmail.com>
2024-03-15 14:21:27 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () =>{
+    return(
+        <div>
+            <h1 className='text-4xl bg-cyan-600'>Knowledge Cafe With React</h1>
+        </div>
+    );
+};
+
+export default Header;
